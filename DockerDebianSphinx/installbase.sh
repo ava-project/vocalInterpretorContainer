621a6f42dfa7fdd454306bbfa21ab294e7ccefaa
David Tran <Opcoz@localhost.localdomain>
2017-08-13 02:44:09 +0200

    [DOCKER][CONTAINER] 	- Created and implemented Dockerfiles for Watson and Sphinx 	- Added Sphinxtrain dependences

---
+++ DockerDebianSphinx/installbase.sh
@@ -0,0 +1,5 @@
+tar -xf sphinxbase-5prealpha.tar.gz
+cd sphinxbase-5prealpha
+./configure
+make
+make install
---
+++ DockerDebianSphinx/installtrain.sh
@@ -0,0 +1,5 @@
+tar -xf sphinxtrain-5prealpha.tar.gz
+cd sphinxtrain-5prealpha
+./configure
+make
+make install
